65f003c12e9f0f8b1465e7eb030a3fc5651196c5
JeongSuIn <suin9711@naver.com>
2020-10-07 17:40:31 +0900

    1005_과제(연산자)
    1005_과제(연산자)

--- 1005Operator/src/operator/Operator2.java
+++ 1005Operator/src/operator/Operator2.java
@@ -4,6 +4,7 @@ public class Operator2 {
 
 	public static void main(String[] args) {
 //		문제 1
+		System.out.println("======문제1======");
 		int x = 2;
 		int y = 5;
 		char c = 'A'; // 'A'의 문자코드는 65.
@@ -18,8 +19,14 @@ public class Operator2 {
 		System.out.println(c++);
 		System.out.println(c);
 
+		
+		
+		
+		
+		
 		System.out.println();
 //		문제 2
+		System.out.println("======문제2======");
 		/*
 		 * ﻿ 아래의 코드는 사과를 담는데 필요한 바구니(버켓)의 수를 구하는 코드이다. 만일 사과의수가 123개이고 하나의 바구니에는10개의 사과를
 		 * 담을 수 있다면,13개의 바구니가 필요할 것이다. (1)에 알맞은 코드를 넣으시오.
@@ -35,8 +42,18 @@ public class Operator2 {
 
 		System.out.println("필요한 바구니의 수: " + numOfBucket);
 
+		System.out.println("-----다른 해결 방안-----");
+		
+		numOfBucket = numOfApples/10 + (numOfApples%10>0?1:0);
+		
+		System.out.println("필요한 바구니의 수2: " + numOfBucket);
+		
+		
+		
+		
 		System.out.println();
 //		문제 3
+		System.out.println("======문제3======");
 		/*
 		 * 아래는 변수num의 값에 따라 ‘양수’,‘음수’,‘0’을 출력하는 코드이다.삼항 연산자를 이용해서(1)에 알맞은 코드를 넣으시오.
 		 * 
@@ -47,8 +64,12 @@ public class Operator2 {
 		int num = 0;
 		System.out.println((num > 0) ? "양수" : (num < 0) ? "음수" : "0");
 
-		System.out.println();
+		
+		
+		
+		System.out.println();		
 //		문제4
+		System.out.println("======문제4======");
 		/*
 		 * 아래는 변수num의 값 중에서 백의 자리 이하를 버리는 코드이다. 만일 변수 num 의 값이‘456’이라면 ‘400’이
 		 * 되고,‘111’이라면 ‘100’이 된다.(1)에 알맞은 코드를 넣으시오.
@@ -56,8 +77,14 @@ public class Operator2 {
 		num = 456;
 		System.out.println("456 -> " + num / 100 * 100);
 
+		
+		
+		
+		
+		
 		System.out.println();
 //		문제 5
+		System.out.println("======문제5======");
 		/*
 		 * 아래의 변수num의 값 중에서 일의 자리를1로 바꾸는 코드이다. 만일 변수 num의값이 333이라면 331이 되고,777이라면 771이
 		 * 된다.(1)에 알맞은 코드를 넣으시오.
@@ -65,8 +92,14 @@ public class Operator2 {
 		num = 333;
 		System.out.println("333 -> " + num / 10 + 1);
 
+		
+		
+		
+		
+		
 		System.out.println();
 //		문제 6
+		System.out.println("======문제6======");
 		/*
 		 * 아래는 변수num의 값보다 크면서도 가장 가까운10의 배수에서 변수num의 값을뺀 나머지를 구하는 코드이다. 예를 들어, 24의 크면서도
 		 * 가장 가까운10의 배수는 30이다.19의 경우 20이고,81의 경우 90이 된다. 30에서 24를 뺀 나머지는6이기 때문에 변수num의
@@ -74,9 +107,19 @@ public class Operator2 {
 		 * 
 		 * [Hint] 나머지 연산자를 사용하라.
 		 */
+		
+		num = 24;
+		
+		System.out.println(10-num%10);
 
+		
+		
+		
+		
+		
 		System.out.println();
 //		문제 7
+		System.out.println("======문제7======");
 		/*
 		 * ﻿ 아래는 화씨(Fahrenheit)를 섭씨(Celcius)로 변환하는 코드이다. 변환공식이 'C =5/9 ×(F - 32)'라고 할
 		 * 때,(1)에 알맞은 코드를 넣으시오. 단, 변환 결과값은 소수점셋째 자리에서 반올림해야 한다.
@@ -85,19 +128,25 @@ public class Operator2 {
 		 */
 
 		int fahrenheit = 100;
-		float celcius = 5 / 9 * (fahrenheit - 32);
+		float celcius = (int)(5/9f*(fahrenheit-32)*100+0.5)/100;
 		System.out.println("Fahrenheit: " + fahrenheit);
 		System.out.println("Celcius: " + celcius);
 
+		
+		
+		
+		
+		
 		System.out.println();
 //		문제 8
+		System.out.println("======문제8======");
 		byte a = 10;
 		byte b = 20;
-		byte c1 = (byte) (a + b);
+		byte c1 = (byte) (a + b); // 1. byte c => int c / 2. (byte)(a+b)
 		char ch = 'A';
-		ch = (char)(ch + 2);
-		float f = (float)3/2;
-		long l = (long)3000 * 3000 * 3000;
+		ch = (char)(ch + 2); // char + int -> int + int = int -> char로 형변환 필요
+		float f = 3f/2; // 3f/2, 3/2f
+		long l = 3000L * 3000 * 3000; // 3000 * 3000L * 3000; / 3000 * 3000 * 3000L; 
 		float f2 = 0.1f;
 		double d = 0.1;
 		boolean result = (float)d==f2;
@@ -109,8 +158,12 @@ public class Operator2 {
 		System.out.println("result="+result);
 		
 		
+		
+		
+		
 		System.out.println();
 //		문제 9
+		System.out.println("======문제9======");
 		
 		/*
 		 *﻿다음은 문자형 변수ch가 영문자(대문자 또는 소문자)이거나 숫자일 때만 변수b의 값이true가 되도록 하는 코드이다.
@@ -119,10 +172,17 @@ public class Operator2 {
 		
 		ch = 'z';
 		boolean b1 = ((48<=ch&&ch<=57) || (65<=ch && ch<=90) || (97<=ch && ch<=122));
-		System.out.println(b1);
+		boolean b2 = (ch>='a'&&ch<='z') || (ch>='A'&&ch<='Z') || (ch>='0'&&ch<='9');
+		System.out.println("b1: " + b1);
+		System.out.println("b2: " + b2);
+		
+		
+		
+		
 		
 		System.out.println();
 //		문제 10
+		System.out.println("======문제10======");
 		/*
 		 * 다음은 대문자를 소문자로 변경하는 코드인데, 문자  ch에 저장된 문자가 대문자인 경우에만 소문자로 변경한다.
 		 * 문자코드는 소문자가 대문자보다 32만큼 더 크다.
@@ -131,6 +191,7 @@ public class Operator2 {
 		
 		ch = 'B';
 		char lowerCase = (65<=ch&&ch<=90)?((char)(ch+32)):ch;
+		// char lowerCase = (ch>='A' && ch<='Z')?((char)(ch+32)):ch;
 		System.out.println("ch: " + ch);
 		System.out.println("ch to lowerCase: " + lowerCase);
 		
