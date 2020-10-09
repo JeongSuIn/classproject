b391e95d64fc0921ba6c69ac678cc6b2cc41a793
JeongSuIn <71997955+JeongSuIn@users.noreply.github.com>
2020-10-09 14:38:56 +0900

    1008_과제_생성자(제출)
    1008_과제(제출)

--- 1008/src/project/PhoneInfor.java
+++ 1008/src/project/PhoneInfor.java
@@ -17,22 +17,22 @@ public class PhoneInfor {
 	
 	// 생성자 추가
 	PhoneInfor(){ //기본 생성자
-		this("가나다", "010-111-1111", null);
+		this("이름", "010-xxxx-xxxx", "2020-01-01");
 	}
 
 	// 생성자의 오버로딩
 	
 	// 이름, 생년월일, 생일
-	PhoneInfor(String id, String num, String day){
-		name = id;
-		phoneNumber = num;
-		birthday = day;
+	PhoneInfor(String name, String phoneNumber, String birthday){
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+		this.birthday = birthday;
 	}
 	
 	// 이름, 생년월일
-	PhoneInfor(String id, String num){
-		name = id;
-		phoneNumber = num;
+	PhoneInfor(String name, String phoneNumber){
+		this.name = name;
+		this.phoneNumber = phoneNumber;
 	}
 	
 	PhoneInfor(PhoneInfor man){
@@ -41,26 +41,25 @@ public class PhoneInfor {
 	
 	
 	
-	// 메소드	
+	// 출력 메소드	
 	void showData() {
-		System.out.println("이름: " + name);
-		System.out.println("전화번호: " + phoneNumber);
-		System.out.println("생일: " + birthday);
+		System.out.println("이름: " + name + ", 전화번호: " + phoneNumber + ", 생일: " + birthday);
+		
 	}
 	
 	
-	// 메인메소드
+	// 메인 메소드
 	public static void main(String[] args) {
 		PhoneInfor man = null;
 		
 		// 사람1
-		man = new PhoneInfor("홍길동", "010-1111-1111", null );
+		man = new PhoneInfor("홍길동", "010-1111-1111" , "2020-01-01");
 		
 		// 사람 2
 		PhoneInfor man2 = new PhoneInfor("일지매", "010-2222-2222", "2020-02-02");
 		
 		// 사람3
-		PhoneInfor man3 = new PhoneInfor("장보고", "010-3333-3333", "2020-03-03");
+		PhoneInfor man3 = new PhoneInfor("장보고", "010-3333-3333");
 		
 		// 출력
 		System.out.println("사람 1의 정보");
