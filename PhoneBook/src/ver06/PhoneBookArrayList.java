386046e5eb080bb5da8a4a235f673c65691d7590
JeongSuIn <suin9711@naver.com>
2020-10-23 17:54:26 +0900

    JavaClass
    JavaClass

---
+++ PhoneBook/src/ver06/PhoneBookArrayList.java
@@ -0,0 +1,18 @@
+package ver06;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PhoneBookArrayList {
+	
+	Scanner sc = new Scanner(System.in);
+	
+	// 생성자 정의
+	private ArrayList<PhoneInfor6> list;
+	
+	public void PhoneInfor6ArrayList() {
+		list =  new ArrayList<PhoneInfor6>(); // 생성자 만들기
+	}
+	
+	
+}
