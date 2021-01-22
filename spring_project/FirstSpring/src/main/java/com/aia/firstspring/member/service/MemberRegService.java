ce330cb9cd4923c13807f3786e44dbeb947cc6e8
JeongSuIn <71997955+JeongSuIn@users.noreply.github.com>
2021-01-22 11:29:10 +0900

    Spring 암호화(21.01.22)

--- spring_project/FirstSpring/src/main/java/com/aia/firstspring/member/service/MemberRegService.java
+++ spring_project/FirstSpring/src/main/java/com/aia/firstspring/member/service/MemberRegService.java
@@ -4,10 +4,9 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.aia.firstspring.Util.AES256Util;
 import com.aia.firstspring.Util.Sha256;
-import com.aia.firstspring.member.dao.MemberDao;
 import com.aia.firstspring.member.dao.MemberInterfaceDao;
-import com.aia.firstspring.member.dao.MybatisMemberDao;
 import com.aia.firstspring.member.domain.Member;
 import com.aia.firstspring.service.MailSenderService;
 
@@ -30,6 +29,9 @@ public class MemberRegService {
 	private Sha256 sha256;
 	
 	@Autowired
+	private AES256Util aes256util;
+	
+	@Autowired
 	private MailSenderService mailSenderService;
 	
 	
@@ -45,6 +47,18 @@ public class MemberRegService {
 			result = 1;
 			
 			System.out.println("암호화: "+sha256.encrypt(member.getPassword()) );
+			
+//			AES256으로 암호화된 문자열: insert or update
+			String epw = aes256util.encrypt(member.getPassword());
+//			AES256으로 복호화된 문자열: select
+			String ppw = aes256util.decrypt(epw);
+			
+			System.out.println("-----------------------------");
+			System.out.println("AES256으로 암호화된 문자열");
+			System.out.println(epw);
+			System.out.println("AES256으로 복호화된 문자열");
+			System.out.println(ppw);
+			
 			
 			String html = "<h1>아래 링크를 통해 인증해주세요. "
 					+ "<a href=\"http//localhost:8080/firstspring\">인증하기</a></h1>";
