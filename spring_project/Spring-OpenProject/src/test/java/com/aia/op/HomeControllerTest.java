d62ca4a6a60173d0a37443c5d30539375c149278
JeongSuIn <71997955+JeongSuIn@users.noreply.github.com>
2021-01-27 11:38:54 +0900

    Spring_OpenProject(21.01.27)
    영속 계층 구현

---
+++ spring_project/Spring-OpenProject/src/test/java/com/aia/op/HomeControllerTest.java
@@ -0,0 +1,16 @@
+package com.aia.op;
+
+
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class HomeControllerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
