e8ebc66d35ed448d4842c1f20fce4c4d2efd6889
JeongSuIn <71997955+JeongSuIn@users.noreply.github.com>
2021-03-19 01:23:01 +0900

    21.03.19(Python_3주차_문자열)

---
+++ Python/Class/section_3_1_021.py
@@ -0,0 +1,19 @@
+# section_3_1_021
+# 문자열 연산
+
+print("="*40)
+
+print("Python은 " + "훌륭한 프로그래밍 언어이다.")
+
+head = "Python은 "
+tail = "훌륭한 프로그래밍 언어이다."
+string = head + tail
+print(string)
+
+str1 = "파"
+str2 = "이"
+str3 = "썬"
+str4 = "짱"
+print(str1 + str2 + str3 + str4)
+
+print("="*40)
---
+++ Python/Class/section_3_1_022.py
@@ -0,0 +1,14 @@
+# section_3_1_022
+# 문자열 인덱싱
+
+string = "PYTHON"
+
+print(string[0])
+print(string[1])
+print(string[5])
+
+print()
+
+print(string[-1])
+print(string[-2])
+print(string[-6])
